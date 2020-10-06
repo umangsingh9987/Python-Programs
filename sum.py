8dd20752d3e7a750f4efcfb1e35b2962762aea31
rahulcodes51 <72370312+rahulcodes51@users.noreply.github.com>
2020-10-06 18:52:33 +0530

    added program for sum of two numbers

---
+++ sum.py
@@ -0,0 +1,7 @@
+#https://m.facebook.com/story.php?story_fbid=348161766403412&id=100036287730638&ref=bookmarks
+#subscribed by Ananda Krishnan Nair
+
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+sum=a+b
+print(sum)
