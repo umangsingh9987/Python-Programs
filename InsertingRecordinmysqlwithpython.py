71b77f3970fd1a1446fa396dc69918c99a3a934f
Ankit Maurya <ankitkumarmaurya000@gmail.com>
2020-10-09 16:34:22 +0530

    By this we can insert data in mysql

---
+++ InsertingRecordinmysqlwithpython.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='electronics',
+                                         user='root',
+                                         password='pynative@#29')
+    mySql_insert_query = """INSERT INTO Laptop (Id, Name, Price, Purchase_date) 
+                           VALUES 
+                           (10, 'Lenovo ThinkPad P71', 6459, '2019-08-14') """
+
+    cursor = connection.cursor()
+    cursor.execute(mySql_insert_query)
+    connection.commit()
+    print(cursor.rowcount, "Record inserted successfully into Laptop table")
+    cursor.close()
+
+except mysql.connector.Error as error:
+    print("Failed to insert record into Laptop table {}".format(error))
+
+finally:
+    if (connection.is_connected()):
+        connection.close()
+        print("MySQL connection is closed")
+
