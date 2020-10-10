24e21777e718a72c281ebeb819b149db88d0d5bb
Rishi Vishwakarma <68197457+VishwakarmaRishi@users.noreply.github.com>
2020-10-10 09:39:44 +0530

    Update ArrayRotation.py

--- ArrayRotation.py
+++ ArrayRotation.py
@@ -1,21 +1,33 @@
-#Function to left rotate arr[] of size n by d*/ 
-def leftRotate(arr, d, n): 
-    for i in range(d): 
-        leftRotatebyOne(arr, n) 
+# #Function to left rotate arr[] of size n by d*/ 
+# def leftRotate(arr, d, n): 
+#     for i in range(d): 
+#         leftRotatebyOne(arr, n) 
   
-#Function to left Rotate arr[] of size n by 1*/  
-def leftRotatebyOne(arr, n): 
-    temp = arr[0] 
-    for i in range(n-1): 
-        arr[i] = arr[i+1] 
-    arr[n-1] = temp 
+# #Function to left Rotate arr[] of size n by 1*/  
+# def leftRotatebyOne(arr, n): 
+#     temp = arr[0] 
+#     for i in range(n-1): 
+#         arr[i] = arr[i+1] 
+#     arr[n-1] = temp 
           
   
-# utility function to print an array */ 
-def printArray(arr,size): 
-    for i in range(size): 
-        print ("%d"% arr[i],end=" ") 
+# # utility function to print an array */ 
+# def printArray(arr,size): 
+#     for i in range(size): 
+#         print ("%d"% arr[i],end=" ") 
   
+    
+    
+# Function to left rotate arr[] by d
+# no need for size of array
+def leftRotate(arr, d):
+    for i in range(d):
+        arr.append(arr.pop(0))        # more short and pythonic way of rotating
+
+# utility function for printing an array        
+def printArray(arr):
+    print(*arr)
+    
    
 # Driver program to test above functions */ 
 arr = [1, 2, 3, 4, 5, 6, 7] 
